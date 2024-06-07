794cd2f361e841079f8fd214d037b83d899df6b9
ferpa <feririarte.1996@gmail.com>
2024-06-07 12:56:07 -0300

    remove eslint-config-turbo on eslint-config

--- packages/eslint-config/library.js
+++ packages/eslint-config/library.js
@@ -1,17 +1,17 @@
-const { resolve } = require('node:path');
+const { resolve } = require("node:path");
 
-const project = resolve(process.cwd(), 'tsconfig.json');
+const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'prettier', 'eslint-config-turbo'],
+  parser: "@typescript-eslint/parser",
+  extends: ["eslint:recommended", "prettier"],
   rules: {
-    curly: 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-non-null-assertion': 0,
-    'react/react-in-jsx-scope': 'off',
-    'react-hooks/exhaustive-deps': 'off',
+    curly: "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-non-null-assertion": 0,
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/exhaustive-deps": "off",
   },
   globals: {
     React: true,
@@ -23,7 +23,7 @@ module.exports = {
     node: true,
   },
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       typescript: {
         project,
       },
@@ -31,13 +31,13 @@ module.exports = {
   },
   ignorePatterns: [
     // Ignore dotfiles
-    '.*.js',
-    'node_modules/',
-    'dist/',
+    ".*.js",
+    "node_modules/",
+    "dist/",
   ],
   overrides: [
     {
-      files: ['*.js?(x)', '*.ts?(x)'],
+      files: ["*.js?(x)", "*.ts?(x)"],
     },
   ],
 };
